ee71fe0d2c3d68f060c5b212e54beaeecf29ab55
mdsrmed <mdsrmed@gmail.com>
2023-07-13 18:23:56 -0400

    HomeView and navigation header

---
+++ CryptoInfo/Core/Home/Views/HomeView.swift
@@ -0,0 +1,67 @@
+//
+//  HomeView.swift
+//  CryptoInfo
+//
+//  Created by Md Shohidur Rahman on 7/13/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            // background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            
+           // content layer
+            VStack {
+               // homeHeader view
+               homeHeader
+                
+               Spacer()
+            }
+            
+            
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack{
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "gearshape")
+                .animation(.none)
+                .background(
+                     CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()){
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
--- CryptoInfo/CryptoInfoApp.swift
+++ CryptoInfo/CryptoInfoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct CryptoInfoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack{
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
