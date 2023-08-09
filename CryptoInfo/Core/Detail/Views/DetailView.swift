5e9c0aece1bb08b80a6af42c0b0119fb79d2d0f1
mdsrmed <mdsrmed@gmail.com>
2023-08-09 17:18:21 -0400

    Render fetched data in grid format with reusable components

--- CryptoInfo/Core/Detail/Views/DetailView.swift
+++ CryptoInfo/Core/Detail/Views/DetailView.swift
@@ -11,12 +11,59 @@ struct DetailView: View {
     
     @StateObject var vm: DetailViewModel
     
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    private let spacing: CGFloat  = 30
+    
     init(coin: CoinModel){
         _vm = StateObject(wrappedValue:DetailViewModel(coin: coin))
     }
     
     var body: some View {
-        Text("It cloudy")
+        ScrollView {
+            VStack(spacing: 20) {
+                Text("")
+                    .frame(height: 150)
+                
+                Text("Overview")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Divider()
+                
+                
+                LazyVGrid(columns: columns,
+                          alignment: .leading,
+                          spacing: spacing) {
+                    ForEach(vm.overviewStatistics){ stat in
+                        StatisticView(stat: stat)
+                    }
+                }
+                
+                Text("Additional Details")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(Color.theme.accent)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Divider()
+                
+                
+                LazyVGrid(columns: columns,
+                          alignment: .leading,
+                          spacing: spacing) {
+                    ForEach(vm.additionalStatistics){ stat in
+                        StatisticView(stat: stat)
+                    }
+                }
+            }
+            .padding()
+        }
+        .navigationTitle(vm.coin.name)
     }
 }
 
